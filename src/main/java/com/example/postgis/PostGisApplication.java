99b050ea8fe5452209bf86fe6782f4d8b8f2f623
bluesun147 <bluesun147@naver.com>
2024-03-25 22:55:33 +0900

    [Init] PostGIS 초기 설정

---
+++ src/main/java/com/example/postgis/PostGisApplication.java
@@ -0,0 +1,13 @@
+package com.example.postgis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostGisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostGisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/postgis/PostGisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postgis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostGisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
